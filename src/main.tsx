ea744e41ec637bfe2bc52b0179edbde450a16349
seobisback <seobisback@gmail.com>
2025-05-09 09:17:16 +0900

    init: initialize vite react + typescript template

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './initialize.css'
+import './global.css'
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
---
+++ src/routes/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+function Home(){
+
+    return (
+        <main className={styles.main}>
+            <section>
+                
+            </section>
+        </main>
+    )
+}
+
+export default Home;
