c76dcc49cd528c05023590119616e19cbc131a0d
seobisback <seobisback@gmail.com>
2025-05-11 22:57:43 +0900

    design: gnb - menu list

--- src/components/shared/GlobalNavigationBar.tsx
+++ src/components/shared/GlobalNavigationBar.tsx
@@ -1,12 +1,14 @@
 import styles from './GlobalNavigationBar.module.css';
 import logo from '../../assets/images/memoji.png';
+import { useNavigate } from 'react-router-dom';
 
 function GlobalNavigationBar(){
+    const navigate = useNavigate();
     return (
         <nav className={styles.nav}>
             <div className={styles.nav_container}>
                 <div className={styles.left_container}>
-                    <div className={styles.title_container}>
+                    <div onClick={() => navigate('/')} className={styles.title_container}>
                         <div className={styles.logo}>
                             <img src={logo} alt="logo" />
                         </div>
@@ -14,7 +16,11 @@ function GlobalNavigationBar(){
                     </div>
                 </div>
                 <div className={styles.right_container}>
-
+                    <ul className={styles.menu_list}>
+                        <li onClick={() => navigate('/')}>Home</li>
+                        <li onClick={() => navigate('/about')}>About</li>
+                        <li onClick={() => navigate('/contact')}>Contact</li>
+                    </ul>
                 </div>
             </div>
         </nav>
