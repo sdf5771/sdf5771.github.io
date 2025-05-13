61a6f08f1a6d6bd0daa1ccdc05b3300ef208c215
seobisback <seobisback@gmail.com>
2025-05-13 09:03:25 +0900

    design: footer layout

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -10,50 +10,52 @@ function Footer() {
                         공부하는 기술, 경험에 대한 이야기를 정리해요.
                     </span>
                 </div>
-                <div className={styles.list}>
-                    <span className={styles.title}>My Socials</span>
-                    <ul>
-                        <li>
-                            <a href="/">
-                                Github
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/">
-                                Instagram
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/">
-                                Facebook
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div className={styles.list}>
-                    <span className={styles.title}>title</span>
-                    <ul>
-                        <li>
-                            <a href="/">
-                                Home
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/">
-                                Home
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div className={styles.list}>
-                    <span className={styles.title}>Contacts</span>
-                    <ul>
-                        <li>
-                            <a href="/">
-                                Email
-                            </a>
-                        </li>
-                    </ul>
+                <div className={styles.lists}>
+                    <div className={styles.list}>
+                        <span className={styles.title}>My Socials</span>
+                        <ul>
+                            <li>
+                                <a href="/">
+                                    Github
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/">
+                                    Instagram
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/">
+                                    Facebook
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.list}>
+                        <span className={styles.title}>title</span>
+                        <ul>
+                            <li>
+                                <a href="/">
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/">
+                                    Home
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.list}>
+                        <span className={styles.title}>Contacts</span>
+                        <ul>
+                            <li>
+                                <a href="/">
+                                    Email
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div className={styles.copyright}>
