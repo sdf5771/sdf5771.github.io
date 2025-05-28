9369b9020700efb734945a5f83e3fa1ef9abc62d
seobisback <seobisback@gmail.com>
2025-05-28 11:28:51 +0900

    feat: typewriter-effect lib 사용

--- src/components/shared/PageTitle.tsx
+++ src/components/shared/PageTitle.tsx
@@ -1,34 +1,19 @@
 import styles from './PageTitle.module.css';
-import { useEffect, useRef } from 'react';
+import Typewriter from 'typewriter-effect';
 
 export default function PageTitle({ title }: { title: string }) {
-    const intervalRef = useRef<NodeJS.Timeout | null>(null);
-    const titleRef = useRef<HTMLHeadingElement>(null);
-
-    useEffect(() => {
-        if (intervalRef.current) {
-            clearInterval(intervalRef.current);
-        }
-
-        intervalRef.current = setInterval(() => {
-            const text = title;
-            for (let i = 0; i < text.length; i++) {
-                setTimeout(() => {
-                    titleRef.current!.textContent = text.slice(0, i + 1);
-                }, i * 100);
-            }
-        }, 1500);
-
-        return () => {
-            if (intervalRef.current) {
-                clearInterval(intervalRef.current);
-            }
-        };
-    }, [title]);
 
     return (
         <div className={styles.title}>
-            <h1 ref={titleRef}>{title.slice(0, 1)}</h1>
+            <Typewriter
+                options={{
+                    strings: [title],
+                    autoStart: true,
+                    loop: true,
+                    wrapperClassName: styles.typewriter,
+                    cursorClassName: styles.cursor,
+                }}
+            />
         </div>
     )
 }
